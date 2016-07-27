c39a96d8f90cfdd94dcb228c489bef29464e7307
agondeau <alexandre.gondeau@gmail.com>
2016-07-27 10:07:07 -0400

    [COSMETIC][CLUSTER] Add empty line

--- src/cluster.c
+++ src/cluster.c
@@ -32,6 +32,7 @@
 /* Macro defines. */
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 #define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
+
 /* -- Enumerations -- */
 
 /** @brief Contains the different distance calculation types.
